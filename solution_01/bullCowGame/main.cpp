477bbf209f99fbd13ce8fb776a9d64df17ac8cdd
Oceane <opere105@fiu.edu>
2019-01-12 18:23:26 -0500

    First lines of Bull-Cow game

---
+++ solution_01/bullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bullCowGame
+//
+//  Created by Peretti Océane on 12/01/2019.
+//  Copyright © 2019 Peretti Océane. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to Bulls and Cows" << endl;
+    return 0;
+}
