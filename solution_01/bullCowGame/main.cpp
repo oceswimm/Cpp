bbede11dfd8a335ac8f33b5771e0b258a844336d
Oceane <opere105@fiu.edu>
2019-01-14 11:31:37 -0500

    implementation of non-magic numbers + learn how to include var within strings

--- solution_01/bullCowGame/main.cpp
+++ solution_01/bullCowGame/main.cpp
@@ -10,6 +10,9 @@
 using namespace std;
 
 int main() {
-    cout << "Welcome to Bulls and Cows" << endl;
+    constexpr int WORD_LENGTH = 5;
+    cout << "Welcome to Bulls and Cows, a fun word game!" << endl;
+    cout << "Can you guess the " <<WORD_LENGTH;
+    cout << " letters isogram I'm thinking of?\n";
     return 0;
 }
