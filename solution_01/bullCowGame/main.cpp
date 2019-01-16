e0188dc98b9a7441489a6f0bbe740d0f2c5d8584
Oceane <opere105@fiu.edu>
2019-01-16 11:21:06 -0500

    Implemented encapsulation to simplify main method getguess to be modified

--- solution_01/bullCowGame/main.cpp
+++ solution_01/bullCowGame/main.cpp
@@ -7,12 +7,38 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+//entry point for the app
 int main() {
+    
+    PrintIntro();
+    GetGuessAndPrintBack();
+    
+    return 0;
+}
+
+//print introduction to the game
+void PrintIntro(){
+    
     constexpr int WORD_LENGTH = 5;
-    cout << "Welcome to Bulls and Cows, a fun word game!" << endl;
+    cout << "Welcome to Bulls and Cows, a fun word game!\n" ;
     cout << "Can you guess the " <<WORD_LENGTH;
     cout << " letters isogram I'm thinking of?\n";
-    return 0;
+    return;
+}
+
+//get guess from player
+string GetGuessAndPrintBack()
+{
+    string Guess = "";
+    cout << "\nEnter your guess: ";
+    getline(cin,Guess);
+    //print the guess back to them
+    cout << "You have typed "<< Guess <<endl;
+    return Guess;
 }
